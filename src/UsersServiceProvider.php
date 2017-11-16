e4e074b94d8ccddaf6d94d64e1f9ab1b560cf931
Marcin Kozak <marcin.ko@modulesgarden.com>
2017-11-16 11:08:32 +0100

    FIX: event labels for notifications

--- src/UsersServiceProvider.php
+++ src/UsersServiceProvider.php
@@ -146,10 +146,10 @@ class UsersServiceProvider extends ModuleServiceProvider
             ->event(UserNotCreated::class, 'Users', 'When user not created')
                 ->addAdminRecipient($adminRecipient)
                 ->register()
-            ->event(UserNotUpdated::class, 'Users', 'When user is updated')
+            ->event(UserNotUpdated::class, 'Users', 'When user not updated')
                 ->addAdminRecipient($adminRecipient)
                 ->register()
-            ->event(UserNotDeleted::class, 'Users', 'When user is deleted')
+            ->event(UserNotDeleted::class, 'Users', 'When user not deleted')
                 ->addAdminRecipient($adminRecipient)
                 ->register();
     }
